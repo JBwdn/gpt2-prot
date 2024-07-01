fffc9aba8cf4fd82fb1638c5dfdc8560a3694099
Jake Bowden <jake.bowden95@gmail.com>
2024-07-01 20:19:39 +0100

    Fix default import route in test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -8,10 +8,10 @@ REF: <https://docs.pytest.org/en/7.4.x/>
 def test_imports():
     """Test that the package and a submodule can be imported."""
 
-    import package  # pylint: disable=import-outside-toplevel
+    import gpt2_prot  # pylint: disable=import-outside-toplevel
 
-    assert package
+    assert gpt2_prot
 
-    import package.scripts  # pylint: disable=import-outside-toplevel
+    import gpt2_prot.scripts  # pylint: disable=import-outside-toplevel
 
-    assert package.scripts
+    assert gpt2_prot.scripts
